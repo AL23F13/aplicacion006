403038c302b6b1f21fbc3549482ffcb3ab820d18
AL23F13 <davidmgyzt@gmail.com>
2015-05-06 14:40:58 -0500

    uno
    remplazo de archivos

---
+++ js/acciones.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#bntvibrar').on('tap',
+	 function(){
+		 navigator.vibrate(1000);
+	 }); //tap 'btnvibrar'
+	 
+	 $('#btnbeep').on('tap',
+	  function(){
+		 navigator.notification.beep(1)
+	 });//Tap 'btnbeep'
+	   
+	$('#izquierda').on('swipeleft',
+	function(){
+		alert("Izquierda");
+	});//Barrido izquierda
+	
+	$('#derecha').on('swiperight',
+	function(){
+		alert("Derecha");
+	});//Barrido izquierda
+	
+	document.addEventListener("pause",function (){
+			$('#listado').append("<p> se pauso </p>");
+	});
+	document.addEventListener("resume",function (){
+			$('#listado').append("<p> se reinicio </p>");
+	});
+	
+	$(window).on('orientationchange',function (e) {
+		$('#listado').append("<p>Orientacion: "+ e.orientation + "</p>");
+	});
+
+
+}); 
+});
+
